3757bd54a8e185a6338920f3df1dab89c5b95cac
Shreya-Jayaraj <119192525+Shreya-Jayaraj@users.noreply.github.com>
2024-02-22 22:43:26 +0530

    add

--- hi.py
+++ hi.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
@@ -14,26 +14,39 @@ db = SQLAlchemy(app)
 class Login(db.Model):
     Username = db.Column(db.String(200), primary_key=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    owner = db.Column(db.String(200), nullable=False)
+    phone_no = db.Column(db.Integer)
 
-#Login(Username = "abhi",password = "Karakkat" , owner = "abhijith")
-
-class UserDetail(db.Model):
+class Item_Qty(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    owner = db.Column(db.String(200), nullable=False)
     item = db.Column(db.String(200), nullable=False)
     qnty = db.Column(db.Integer)
 
-class Item(db.Model):
+class Item_Details(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item = db.Column(db.String(200), nullable=False)
     item_image  = db.Column(db.String(600),nullable = False)
     prod_desc = db.Column(db.String(500))
+    owner_uname = db.Column(db.String(200),nullable = False)
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        '''new_login = Login(Username="abhi", password="Karakkat", owner="abhijith")
+        new_login = Login(Username="abhi", password="Karakkat", owner="abhijith")
         db.session.add(new_login)
-        db.session.commit()'''
+        db.session.commit()
     app.run(debug=True)
+
+@app.route('/create-item', methods=['POST'])
+def create_item():
+    data = request.get_json()
+
+    new_item = Item_Details(
+        id = data.get('id'),
+        item = data.get('item'),
+        item_image = data.get('item_image'),
+        prod_desc = data.get('prod_desc'),
+        owner_uname = data.get('owner_uname')
+                            )
+    
+    db.session.add(new_item)
+    db.session.commit()
