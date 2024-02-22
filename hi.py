d255b9afc9130f06edc891f47233e5c0bf486e82
[abhijithkbijumon] <[abhijithkbijumon@gmail.com]>
2024-02-22 22:51:12 +0530

    sdfsdf

--- hi.py
+++ hi.py
@@ -14,13 +14,13 @@ db = SQLAlchemy(app)
 class Login(db.Model):
     Username = db.Column(db.String(200), primary_key=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    owner = db.Column(db.String(200), nullable=False)
+    phone_no = db.Column(db.Integer)
+
 
 #Login(Username = "abhi",password = "Karakkat" , owner = "abhijith")
 
-class UserDetail(db.Model):
+class Item_Qty(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    owner = db.Column(db.String(200), nullable=False)
     item = db.Column(db.String(200), nullable=False)
     qnty = db.Column(db.Integer)
 
@@ -29,11 +29,13 @@ class Item(db.Model):
     item = db.Column(db.String(200), nullable=False)
     item_image  = db.Column(db.String(600),nullable = False)
     prod_desc = db.Column(db.String(500))
+    owner_uname = db.Column(db.String(200),nullable = False)
+
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        '''new_login = Login(Username="abhi", password="Karakkat", owner="abhijith")
+        '''new_login = Login(Username="abhi", password="Karakkat", phone_no="9188037677")
         db.session.add(new_login)
         db.session.commit()'''
     app.run(debug=True)
